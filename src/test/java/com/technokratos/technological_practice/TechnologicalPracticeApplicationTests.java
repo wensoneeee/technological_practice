6fdb0afcee9eb81777044720fb6f554b64402f6a
wensoneeee <arturinrussia@gmail.com>
2026-04-05 10:51:20 +0300

    первоначальная настройка

---
+++ src/test/java/com/technokratos/technological_practice/TechnologicalPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technokratos.technological_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologicalPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
