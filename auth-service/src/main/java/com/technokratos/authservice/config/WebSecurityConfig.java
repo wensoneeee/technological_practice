b7ee538831d9994735f96193ab830df404e70e94
DilyaZi <diller.zeegee@gmail.com>
2026-05-25 22:10:16 +0300

    swagger booking service

--- auth-service/src/main/java/com/technokratos/authservice/config/WebSecurityConfig.java
+++ auth-service/src/main/java/com/technokratos/authservice/config/WebSecurityConfig.java
@@ -21,8 +21,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
-    private static final String[] PERMIT_ALL_URLS = {"/api/v1/auth/**", "/error", "/swagger-ui/**",             // Добавьте это
-            "/v3/api-docs", "/webjars/**"};
+    private static final String[] PERMIT_ALL_URLS = {"/api/v1/auth/**", "/error", "/v3/api-docs"};
 
     private final JwtTokenFilter jwtTokenFilter;
 
