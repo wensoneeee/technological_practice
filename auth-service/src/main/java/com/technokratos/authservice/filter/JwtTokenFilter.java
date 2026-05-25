54566c59d78008f2534be86dec460aae141d958b
DilyaZi <diller.zeegee@gmail.com>
2026-05-26 00:33:14 +0300

    auth-service swagger 1.0

--- auth-service/src/main/java/com/technokratos/authservice/filter/JwtTokenFilter.java
+++ auth-service/src/main/java/com/technokratos/authservice/filter/JwtTokenFilter.java
@@ -24,6 +24,12 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String header = request.getHeader("Authorization");
 
         if (header != null && header.startsWith("Bearer ")) {
